c68f4db715b01232056caa4d5c43945096dfae66
Jed Lynch <jedlynch@gmail.com>
2017-08-23 21:59:36 -0400

    First push

---
+++ app/test.php
@@ -0,0 +1,10 @@
+<?php
+namespace app {
+	class test{
+ 		
+ 		public function __construct(){
+ 			echo "<H1> SHIT WORKS </H1>";
+ 		}
+
+	}
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+	#Run AutoLoad
+	#require(__DIR__ . '/app/autoload.php');
+	require(__DIR__.'/vendor/autoload.php');
+	
+	#Include Database Credentials
+	#require(__DIR__ . '/app/database.php');
+
+
+
+	################  TEST         ##################
+	new \app\test();
+
+
+	################  Running Code ##################
